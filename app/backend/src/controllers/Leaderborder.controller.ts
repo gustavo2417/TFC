58475eab71106a1021b87bff671c332448853bf3
Gustavo <gustavodarocha241@gmail.com>
2023-02-13 18:34:17 -0300

    feat: requisitos 31 e 32

--- app/backend/src/controllers/Leaderborder.controller.ts
+++ app/backend/src/controllers/Leaderborder.controller.ts
@@ -1,19 +1,34 @@
 import { Request, Response } from 'express';
-import leaderboardServices from '../services/Leaderboard.service';
+import leaderboardHome from '../services/Leaderboard.service';
+import leaderboardAway from '../services/leaderboardAway.service';
+import Leadeboard from '../interfaces/Ileaderboard';
 
-const getLeaderboardHome = async (_req: Request, res: Response) => {
-  const result = await leaderboardServices.leaderboardHome();
-
-  result.sort((a, b) =>
+const sort = (results: Leadeboard[]) => {
+  const newSort = results.sort((a, b) =>
     b.totalPoints - a.totalPoints
   || b.totalVictories - a.totalVictories
   || b.goalsBalance - a.goalsBalance
   || b.goalsFavor - a.goalsFavor
   || b.goalsOwn - a.goalsOwn);
 
+  return newSort;
+};
+
+const getLeaderboardHome = async (_req: Request, res: Response) => {
+  const leadeboard = await leaderboardHome.leaderboardHome();
+  const result = sort(leadeboard);
+
+  return res.status(200).json(result);
+};
+
+const getLeaderboardAway = async (_req: Request, res: Response) => {
+  const leadeboard = await leaderboardAway.leaderboardAway();
+  const result = sort(leadeboard);
+
   return res.status(200).json(result);
 };
 
 export default {
   getLeaderboardHome,
+  getLeaderboardAway,
 };
---
+++ app/backend/src/services/leaderboardAway.service.ts
@@ -0,0 +1,66 @@
+import teamsModel from '../database/models/Teams.model';
+import matchesModel from '../database/models/Matches.model';
+import leadeboard from '../interfaces/Ileaderboard';
+import Imatch from '../interfaces/IreturnedMatch';
+
+const tableTeam = {
+  name: '',
+  totalPoints: 0,
+  totalGames: 0,
+  totalVictories: 0,
+  totalDraws: 0,
+  totalLosses: 0,
+  goalsFavor: 0,
+  goalsOwn: 0,
+  goalsBalance: 0,
+  efficiency: 0,
+};
+
+const fillTable = async (team: leadeboard, matches: Imatch[]) => {
+  const result = matches.reduce((acc: leadeboard, current: Imatch) => {
+    acc.totalGames += 1;
+    if (current.awayTeamGoals > current.homeTeamGoals) {
+      acc.totalVictories += 1;
+      acc.totalPoints += 3;
+    } if (current.awayTeamGoals === current.homeTeamGoals) {
+      acc.totalDraws += 1;
+      acc.totalPoints += 1;
+    } if (current.awayTeamGoals < current.homeTeamGoals) {
+      acc.totalLosses += 1;
+    }
+    acc.goalsFavor += current.awayTeamGoals;
+    acc.goalsOwn += current.homeTeamGoals;
+    acc.goalsBalance = (acc.goalsFavor - acc.goalsOwn);
+    acc.efficiency = ((acc.totalPoints / (acc.totalGames * 3)) * 100).toFixed(2);
+
+    return acc;
+  }, team);
+
+  return result;
+};
+
+const leaderboardAway = async () => {
+  const allTeams = await teamsModel.findAll();
+
+  const newTablesEmpty = allTeams.map((team) => {
+    const allTableTeams = {
+      ...tableTeam,
+      name: team.teamName,
+    };
+    return allTableTeams;
+  });
+
+  const allMatchsTeams = await Promise.all(allTeams.map(async (teams, index) => {
+    const matches = await matchesModel.findAll(
+      { where: { awayTeamId: teams.id, inProgress: false } },
+    );
+
+    const result = fillTable(newTablesEmpty[index], matches);
+
+    return result;
+  }));
+
+  return allMatchsTeams;
+};
+
+export default { leaderboardAway };
